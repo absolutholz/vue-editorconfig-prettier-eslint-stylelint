f4f28feb7c953d1d548e4db2707e3a3921602a5a
root <root@Dell-XPS-13.localdomain>
2020-05-30 23:31:02 +0200

    Add and configure eslint & prettier for vue cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+	root: true,
+
+	env: {
+		browser: true,
+		es2020: true,
+		node: true
+	},
+
+	extends: ["plugin:vue/essential", "eslint:recommended", "prettier"],
+
+	parserOptions: {
+		parser: "babel-eslint"
+	},
+
+	rules: {
+		"prettier/prettier": "error",
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+	},
+
+	plugins: ["prettier"]
+};
