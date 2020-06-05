8e6a81a8790e540a3ae330ce78ad51bd1b263782
root <root@Dell-XPS-13.localdomain>
2020-06-05 07:28:36 +0200

    Update stylelint to fix vue files (no cli plugin)

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
 	root: true,
 
-	extends: "stylelint-config-standard"
+	// extends: ["stylelint-config-standard", "stylelint-config-prettier"],
+	extends: [
+		"stylelint-config-recommended",
+		"stylelint-config-sass-guidelines",
+		"stylelint-prettier/recommended",
+	],
 };
